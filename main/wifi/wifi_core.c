6dcfabe768baaf7c269de5a460238aa969ca2cef
Pankaj1215 <pankaj.sharma@audertec.com>
2020-09-14 20:01:16 +0530

    publishSubscribeAdded

--- main/wifi/wifi_core.c
+++ main/wifi/wifi_core.c
@@ -56,8 +56,7 @@
 
 #ifdef P_TESTING
 
-#define thing_Shadow   // for define thing shadow
-
+// #define thing_Shadow   // for define thing shadow
 #ifdef thing_Shadow
 #include "aws_iot_config.h"
 #include "aws_iot_log.h"
@@ -65,6 +64,17 @@
 #include "aws_iot_mqtt_client_interface.h"
 #include "aws_iot_shadow_interface.h"
 #endif
+
+#define subscribePublishTopic
+#ifdef subscribePublishTopic
+#include "aws_iot_config.h"
+#include "aws_iot_log.h"
+#include "aws_iot_version.h"
+#include "aws_iot_mqtt_client_interface.h"
+#include "aws_iot_shadow_interface.h"
+#endif
+
+// #define HTTP_CLIENT_Code
 
  #include "esp_http_client.h"    // New Added Header Files for HTTP File..28Aug2020//
  // #define DATA_HTTP_BIN_ORG                               // For Showing Data On httpbin.org
@@ -612,8 +622,6 @@ static void http_get_task(void *pvParameters)
 
 
 
-
-//#define HTTP_CLIENT_Code
 
 #ifdef P_TESTING    //    // New Added For HTTP_PS_20
 
@@ -1323,6 +1331,244 @@ void http_test_task(void *pvParameters)
  }
 
 #endif
+
+
+
+
+
+#ifdef subscribePublishTopic
+
+
+ static const char *TAG = "subpub";
+
+ /* The examples use simple WiFi configuration that you can set via
+    'make menuconfig'.
+    If you'd rather not, just change the below entries to strings with
+    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
+ */
+ #define EXAMPLE_WIFI_SSID CONFIG_WIFI_SSID
+ #define EXAMPLE_WIFI_PASS CONFIG_WIFI_PASSWORD
+
+ /* FreeRTOS event group to signal when we are connected & ready to make a request */
+ static EventGroupHandle_t wifi_event_group;
+
+ /* The event group allows multiple bits for each event,
+    but we only care about one event - are we connected
+    to the AP with an IP? */
+// const int CONNECTED_BIT = BIT0;   // commented for merge code..
+
+
+ /* CA Root certificate, device ("Thing") certificate and device
+  * ("Thing") key.
+    Example can be configured one of two ways:
+    "Embedded Certs" are loaded from files in "certs/" and embedded into the app binary.
+    "Filesystem Certs" are loaded from the filesystem (SD card, etc.)
+    See example README for more details.
+ */
+ #if defined(CONFIG_EXAMPLE_EMBEDDED_CERTS)
+
+ extern const uint8_t aws_root_ca_pem_start[] asm("_binary_aws_root_ca_pem_start");
+ extern const uint8_t aws_root_ca_pem_end[] asm("_binary_aws_root_ca_pem_end");
+ extern const uint8_t certificate_pem_crt_start[] asm("_binary_certificate_pem_crt_start");
+ extern const uint8_t certificate_pem_crt_end[] asm("_binary_certificate_pem_crt_end");
+ extern const uint8_t private_pem_key_start[] asm("_binary_private_pem_key_start");
+ extern const uint8_t private_pem_key_end[] asm("_binary_private_pem_key_end");
+
+ #elif defined(CONFIG_EXAMPLE_FILESYSTEM_CERTS)
+
+ static const char * DEVICE_CERTIFICATE_PATH = CONFIG_EXAMPLE_CERTIFICATE_PATH;
+ static const char * DEVICE_PRIVATE_KEY_PATH = CONFIG_EXAMPLE_PRIVATE_KEY_PATH;
+ static const char * ROOT_CA_PATH = CONFIG_EXAMPLE_ROOT_CA_PATH;
+
+ #else
+ #error "Invalid method for loading certs"
+ #endif
+
+ /**
+  * @brief Default MQTT HOST URL is pulled from the aws_iot_config.h
+  */
+ char HostAddress[255] = AWS_IOT_MQTT_HOST;
+
+ /**
+  * @brief Default MQTT port is pulled from the aws_iot_config.h
+  */
+ uint32_t port = AWS_IOT_MQTT_PORT;
+
+
+ void iot_subscribe_callback_handler(AWS_IoT_Client *pClient, char *topicName, uint16_t topicNameLen,
+                                     IoT_Publish_Message_Params *params, void *pData) {
+     ESP_LOGI(TAG, "Subscribe callback");
+     ESP_LOGI(TAG, "%.*s\t%.*s", topicNameLen, topicName, (int) params->payloadLen, (char *)params->payload);
+
+     printf(" params->payload) %s \n ", (char*) params-> payload);
+ }
+
+ void disconnectCallbackHandler(AWS_IoT_Client *pClient, void *data) {
+     ESP_LOGW(TAG, "MQTT Disconnect");
+     IoT_Error_t rc = FAILURE;
+
+     if(NULL == pClient) {
+         return;
+     }
+
+     if(aws_iot_is_autoreconnect_enabled(pClient)) {
+         ESP_LOGI(TAG, "Auto Reconnect is enabled, Reconnecting attempt will start now");
+     } else {
+         ESP_LOGW(TAG, "Auto Reconnect not enabled. Starting manual reconnect...");
+         rc = aws_iot_mqtt_attempt_reconnect(pClient);
+         if(NETWORK_RECONNECTED == rc) {
+             ESP_LOGW(TAG, "Manual Reconnect Successful");
+         } else {
+             ESP_LOGW(TAG, "Manual Reconnect Failed - %d", rc);
+         }
+     }
+ }
+
+
+ void aws_iot_task(void *param) {
+     char cPayload[100];
+
+     int32_t i = 0;
+
+     IoT_Error_t rc = FAILURE;
+
+     AWS_IoT_Client client;
+     IoT_Client_Init_Params mqttInitParams = iotClientInitParamsDefault;
+     IoT_Client_Connect_Params connectParams = iotClientConnectParamsDefault;
+
+     IoT_Publish_Message_Params paramsQOS0;
+     IoT_Publish_Message_Params paramsQOS1;
+
+     ESP_LOGI(TAG, "AWS IoT SDK Version %d.%d.%d-%s", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_TAG);
+
+     mqttInitParams.enableAutoReconnect = false; // We enable this later below
+     mqttInitParams.pHostURL = HostAddress;
+     mqttInitParams.port = port;
+
+ #if defined(CONFIG_EXAMPLE_EMBEDDED_CERTS)
+     mqttInitParams.pRootCALocation = (const char *)aws_root_ca_pem_start;
+     mqttInitParams.pDeviceCertLocation = (const char *)certificate_pem_crt_start;
+     mqttInitParams.pDevicePrivateKeyLocation = (const char *)private_pem_key_start;
+
+ #elif defined(CONFIG_EXAMPLE_FILESYSTEM_CERTS)
+     mqttInitParams.pRootCALocation = ROOT_CA_PATH;
+     mqttInitParams.pDeviceCertLocation = DEVICE_CERTIFICATE_PATH;
+     mqttInitParams.pDevicePrivateKeyLocation = DEVICE_PRIVATE_KEY_PATH;
+ #endif
+
+     mqttInitParams.mqttCommandTimeout_ms = 20000;
+     mqttInitParams.tlsHandshakeTimeout_ms = 5000;
+     mqttInitParams.isSSLHostnameVerify = true;
+     mqttInitParams.disconnectHandler = disconnectCallbackHandler;
+     mqttInitParams.disconnectHandlerData = NULL;
+
+ #ifdef CONFIG_EXAMPLE_SDCARD_CERTS
+     ESP_LOGI(TAG, "Mounting SD card...");
+     sdmmc_host_t host = SDMMC_HOST_DEFAULT();
+     sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
+     esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+         .format_if_mount_failed = false,
+         .max_files = 3,
+     };
+     sdmmc_card_t* card;
+     esp_err_t ret = esp_vfs_fat_sdmmc_mount("/sdcard", &host, &slot_config, &mount_config, &card);
+     if (ret != ESP_OK) {
+         ESP_LOGE(TAG, "Failed to mount SD card VFAT filesystem. Error: %s", esp_err_to_name(ret));
+         abort();
+     }
+ #endif
+
+     rc = aws_iot_mqtt_init(&client, &mqttInitParams);
+     if(SUCCESS != rc) {
+         ESP_LOGE(TAG, "aws_iot_mqtt_init returned error : %d ", rc);
+         abort();
+     }
+
+     /* Wait for WiFI to show as connected */
+     xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT,
+                         false, true, portMAX_DELAY);
+
+     connectParams.keepAliveIntervalInSec = 10;
+     connectParams.isCleanSession = true;
+     connectParams.MQTTVersion = MQTT_3_1_1;
+     /* Client ID is set in the menuconfig of the example */
+     connectParams.pClientID = CONFIG_AWS_EXAMPLE_CLIENT_ID;
+     connectParams.clientIDLen = (uint16_t) strlen(CONFIG_AWS_EXAMPLE_CLIENT_ID);
+     connectParams.isWillMsgPresent = false;
+
+     ESP_LOGI(TAG, "Connecting to AWS...");
+     do {
+         rc = aws_iot_mqtt_connect(&client, &connectParams);
+         if(SUCCESS != rc) {
+             ESP_LOGE(TAG, "Error(%d) connecting to %s:%d", rc, mqttInitParams.pHostURL, mqttInitParams.port);
+             vTaskDelay(1000 / portTICK_RATE_MS);
+         }
+     } while(SUCCESS != rc);
+
+     /*
+      * Enable Auto Reconnect functionality. Minimum and Maximum time of Exponential backoff are set in aws_iot_config.h
+      *  #AWS_IOT_MQTT_MIN_RECONNECT_WAIT_INTERVAL
+      *  #AWS_IOT_MQTT_MAX_RECONNECT_WAIT_INTERVAL
+      */
+     rc = aws_iot_mqtt_autoreconnect_set_status(&client, true);
+     if(SUCCESS != rc) {
+         ESP_LOGE(TAG, "Unable to set Auto Reconnect to true - %d", rc);
+         abort();
+     }
+
+     const char *TOPIC = "test_topic/esp32";
+     const int TOPIC_LEN = strlen(TOPIC);
+
+     ESP_LOGI(TAG, "Subscribing...");
+     rc = aws_iot_mqtt_subscribe(&client, TOPIC, TOPIC_LEN, QOS0, iot_subscribe_callback_handler, NULL);
+     if(SUCCESS != rc) {
+         ESP_LOGE(TAG, "Error subscribing : %d ", rc);
+         abort();
+     }
+
+     sprintf(cPayload, "%s : %d ", "hello from SDK", i);
+
+     paramsQOS0.qos = QOS0;
+     paramsQOS0.payload = (void *) cPayload;
+     paramsQOS0.isRetained = 0;
+
+     paramsQOS1.qos = QOS1;
+     paramsQOS1.payload = (void *) cPayload;
+     paramsQOS1.isRetained = 0;
+
+     while((NETWORK_ATTEMPTING_RECONNECT == rc || NETWORK_RECONNECTED == rc || SUCCESS == rc)) {
+
+         //Max time the yield function will wait for read messages
+         rc = aws_iot_mqtt_yield(&client, 100);
+         if(NETWORK_ATTEMPTING_RECONNECT == rc) {
+             // If the client is attempting to reconnect we will skip the rest of the loop.
+             continue;
+         }
+
+         ESP_LOGI(TAG, "Stack remaining for task '%s' is %d bytes", pcTaskGetTaskName(NULL), uxTaskGetStackHighWaterMark(NULL));
+         vTaskDelay(1000 / portTICK_RATE_MS);
+         sprintf(cPayload, "%s : %d ", "hello from ESP32 (QOS0)", i++);
+         paramsQOS0.payloadLen = strlen(cPayload);
+         rc = aws_iot_mqtt_publish(&client, TOPIC, TOPIC_LEN, &paramsQOS0);
+
+         sprintf(cPayload, "%s : %d ", "hello from ESP32 (QOS1)", i++);
+         paramsQOS1.payloadLen = strlen(cPayload);
+         rc = aws_iot_mqtt_publish(&client, TOPIC, TOPIC_LEN, &paramsQOS1);
+         if (rc == MQTT_REQUEST_TIMEOUT_ERROR) {
+             ESP_LOGW(TAG, "QOS1 publish ack not received.");
+             rc = SUCCESS;
+         }
+     }
+
+     ESP_LOGE(TAG, "An error occurred in the main loop.");
+     abort();
+ }
+
+
+#endif  // end for subscribe and publish..
+
+
+
 
 
 
